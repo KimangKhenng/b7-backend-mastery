b22c14a01503211301c9786493058679acf8ca3a
Kim Ang Kheang <kimangkheang@gmail.com>
2026-07-08 13:14:55 +0700

    D2

---
+++ d2/index.js
@@ -0,0 +1,13 @@
+import express from 'express';
+
+const app = express();
+const PORT = 3000
+
+app.get('/', (req, res) => {
+    console.log(req.ip)
+    res.status(201).json({ message: "Hello World 3" })
+})
+
+app.listen(PORT, () => {
+    console.log(`Server running on port: ${PORT}`)
+});
